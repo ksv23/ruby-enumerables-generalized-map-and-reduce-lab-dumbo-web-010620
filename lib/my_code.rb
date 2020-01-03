63f3fd1eda58fa2fcae7ef9a248704c67acbb857
Kring Villanueva <ksvillanueva7@gmail.com>
2020-01-03 00:13:34 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,34 @@
 # Your Code Here
+def map(source_array)
+  new = []
+  x = 0
+
+  while x < source_array.length do
+    new.push(yield(source_array[x]))
+    x += 1
+  end
+  # source_array.map { |source_array| source_array * -1}
+  new
+end
+
+
+def reduce(source_array, starting_value = nil)
+
+  if starting_value
+    sum = starting_value
+    x = 0
+  else
+    sum = source_array[0]
+    x = 1
+  end
+
+  while x < source_array.length do
+    sum = yield(sum, source_array[x])
+    x += 1
+  end
+
+  sum
+
+end
+
+# reduce([1,2,3], 100)
